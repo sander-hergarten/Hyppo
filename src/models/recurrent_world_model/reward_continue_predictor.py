0dc9262135d9ebe3c2b5eb6a2ea58766d5a09f72
sander-hergarten <mail@sander-hergarten.de>
2023-03-03 18:15:31 +0100

    DEV: added reward predictor

--- src/models/recurrent_world_model/reward_continue_predictor.py
+++ src/models/recurrent_world_model/reward_continue_predictor.py
@@ -1,33 +1,3 @@
-from tensorflow.keras import layers
-from tensorflow.keras import Model
-
-from .utils import symexp
-
-
-class RewardPredictor(Model):
-    def __init__(self, fc_layers: int = 1, recurrent_state_size=1024):
-        super().__init__()
-
-        self.input = layers.Dense("relu", recurrent_state_size)
-
-        self.fully_connected_layers = [
-            layers.Dense("relu", 512) for _ in range(fc_layers)
-        ]
-
-        self.output_layer = layers.Dense("linear", 1)
-
-    def call_no_symexp(self, x):
-        intermediate_result = self.input(x)
-
-        for layer in self.fully_connected_layers:
-            intermediate_result = layer(intermediate_result)
-
-        output = self.output_layer(intermediate_result)
-
-        return output
-
-    def call(self, x):
-        return symexp(self.call_no_symlog(x))
 
 
 class ContinuePredictor(Model):
