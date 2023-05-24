DEV: fixed gradient tape

src/models/recurrent_world_model/auxilary_output.py
@@ -4,7 +4,7 @@ from tensorflow.keras import Model, layers
 from utils import config, symexp, symlog_loss
 
 
-class RewardPredictor(Model):
+class RewardPredictor(tf.keras.Model):
     def __init__(self):
         super().__init__()
 
@@ -38,7 +38,7 @@ class RewardPredictor(Model):
         return symexp(self.call_no_symlog(x))
 
     @tf.function
-    def loss(self, x, y):
+    def loss_fn(self, x, y):
         y_pred = self.call_no_symexp(x)
 
         return symlog_loss(y, y_pred)
@@ -57,10 +57,10 @@ class ContinuePredictor(Model):
 
         self.output_layer = layers.Dense(1, "sigmoid")
 
-        self.binary_crossentropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        self.binary_crossentropy = tf.keras.losses.BinaryCrossentropy()
 
     def call(self, x):
-        intermediate_result = self.input(x)
+        intermediate_result = x
 
         for layer in self.fully_connected_layers:
             intermediate_result = layer(intermediate_result)
@@ -71,8 +71,11 @@ class ContinuePredictor(Model):
         return sampler.sample(1)
 
     @tf.function
-    def loss(self, x, y):
-        y_pred = self(x)
+    def loss_fn(self, x, y):
+        y_pred = self(x)[0]
+
+        y_pred = tf.cast(y_pred, tf.float32)
+
         return self.binary_crossentropy(y, y_pred)
 
 
@@ -99,7 +102,7 @@ class Decoder(Model):
 
         self.cnn_layers = [
             layers.Conv2DTranspose(
-                2**i * cnn_depth,
+                3,
                 kernel,
                 activation=activation,
                 strides=cnn_stride,
@@ -121,12 +124,15 @@ class Decoder(Model):
         for layer in self.cnn_layers:
             encoded_observation = layer(encoded_observation)
 
+        return encoded_observation
+
     def call(self, x):
         return symexp(self.call_no_symexp(x))
 
-    @tf.function
-    def loss(self, x, y):
+    def loss_fn(self, x, y):
         y_pred = self.call_no_symexp(x)
         processed_y = self.resizing_layer(y)
 
-        return symlog_loss(processed_y, y_pred)
+        k = symlog_loss(processed_y, y_pred)
+
+        return k

src/models/recurrent_world_model/sequence_model.py
@@ -32,6 +32,8 @@ class Encoder(Model):
         self.distribution_reshape = layers.Reshape((32, 32), input_shape=(32 * 32,))
         self.resizing_layer = layers.Resizing(64, 64)
 
+        self.flatten_layer = layers.Flatten()
+
     def observation_preprocessing(self, observation):
         processed_observation = self.resizing_layer(observation)
         processed_observation = symlog(processed_observation)
@@ -64,7 +66,9 @@ class Encoder(Model):
         return distribution
 
     def sample(self, distribution):
-        categorical_one_hot_vector = tf.map_fn(one_hot, distribution)
+        categorical_one_hot_vector = self.flatten_layer(
+            tf.map_fn(one_hot, distribution)
+        )
 
         return categorical_one_hot_vector
 
@@ -137,8 +141,6 @@ class SequenceModel(tf.keras.Model):
         state = tf.stack([recurrent_state])
 
         for layer in self.gru_layers:
-            print("state", state)
-
             intermediate_recurrent, state = layer(intermediate_recurrent, state)
 
         intermediate_dense = intermediate_recurrent

src/models/recurrent_world_model/utils.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
+import pathlib
 import yaml
 
 symlog = lambda x: tf.sign(x) * tf.math.log(tf.abs(x) + 1)
@@ -36,8 +37,10 @@ def symlog_loss(y_true, y_pred):
     return 0.5 * (y_pred - symlog(y_true)) ** 2
 
 
+config_path = pathlib.Path.cwd() / "config.yaml"
+
 with open(
-    "/home/sanderhergarten/Hyppo/src/models/recurrent_world_model/config.yaml",
+    config_path,
     "r",
 ) as stream:
     try:

src/models/recurrent_world_model/world_model.py
@@ -18,11 +18,8 @@ class WorldModel(Model):
     reward_predictor = RewardPredictor()
     decoder = Decoder()
 
-    recurrent_state = {
-        "timestep": 0,
-        "value": tf.zeros((config["model_parameters"]["recurrent_state_size"])),
-    }
-    stochastic_state = {"timestep": 0, "value": tf.zeros((32 * 32))}
+    recurrent_state = (tf.zeros((config["model_parameters"]["recurrent_state_size"])),)
+    stochastic_state = tf.zeros((32 * 32))
 
     training = False
 
@@ -45,113 +42,181 @@ class WorldModel(Model):
 
         dataset_iterator = iter(dataset)
 
-        for image, reward, continue_flag, action in dataset_iterator:
+        for observation, reward, continue_flag, action in dataset_iterator:
             action = self.process_action(action)
 
-            with tf.GradientTape() as tape:
-                tape.watch(image)
-                tape.watch(action)
-                self.advance_recurrent_state(action)
+            with tf.GradientTape(persistent=True) as tape:
+                self.recurrent_state = self.sequence_model(
+                    recurrent_state=self.recurrent_state,
+                    stochastic_state=self.stochastic_state,
+                    action=action,
+                )[0]
 
-                (
-                    dynamics_loss,
-                    representation_loss,
-                ) = self.stochastic_timestep_to_recurrent_timestep(image)
+                stochastic_dist = self.encoder.distribution(
+                    self.recurrent_state, observation
+                )
+                res = self.dynamics_predictor.distribution(self.recurrent_state)
 
-                print(dynamics_loss, representation_loss)
-                # continue_loss = -tf.math.log(
-                #     self.continue_predictor.loss(self.model_state, continue_flag)
-                # )
-                # reward_loss = -tf.math.log(
-                #     self.reward_predictor.loss(self.model_state, reward)
-                # )
-                # decoder_loss = -tf.math.log(self.decoder.loss(self.model_state, image))
+                self.stochastic_state = self.encoder.sample(stochastic_dist)
 
-                # prediction_loss = reward_loss + continue_loss + decoder_loss
+                loss_dynamics = self.kl_divergence(
+                    tf.stop_gradient(stochastic_dist), res
+                )
+                loss_representation = self.kl_divergence(
+                    tf.stop_gradient(res), stochastic_dist
+                )
 
-                # loss = (
-                #     DISCOUNTS["PRED"] * prediction_loss
-                #     + DISCOUNTS["DYN"] * dynamics_loss
-                #     + DISCOUNTS["REP"] * representation_loss
-                # )
+                model_state = tf.concat(
+                    [self.recurrent_state, self.stochastic_state], axis=1
+                )
 
-                loss = dynamics_loss
+                loss_decoder = -tf.math.log(decoder.loss_fn(model_state, observation))
+                loss_reward = -tf.math.log(
+                    reward_predictor.loss_fn(model_state, reward)
+                )
+                loss_continue = -tf.math.log(
+                    continue_predictor.loss_fn(model_state, continue_flag)
+                )
 
-            for ellement in [
+                loss_prediction = loss_decoder + loss_reward + loss_continue
+
+                loss_dynamics = tf.math.maximum(1.0, loss_dynamics)
+                loss_representation = tf.math.maximum(1.0, loss_representation)
+
+                loss = (
+                    0.5 * loss_dynamics
+                    + 0.1 * loss_representation
+                    + 1 * loss_prediction
+                )
+
+            grad_var_pairs = []
+
+            for model in [
                 self.dynamics_predictor,
+                self.encoder,
+                self.sequence_model,
+                self.decoder,
+                self.reward_predictor,
+                self.continue_predictor,
             ]:
-                variables = ellement.trainable_variables
-                gradient = tape.gradient(loss, variables)
+                variables = model.trainable_variables
+                grads = tape.gradient(loss, variables)
 
-                self.optimizer.apply_gradients(zip(gradient, variables))
+                grad_var_pairs.extend(list(zip(grads, variables)))
 
-    @property
-    def model_state(self):
-        return tf.concat([self.recurrent_state, self.stochastic_state], axis=1)
+            self.optimizer.apply_gradients(grad_var_pairs)
 
-    def process_action(self, p_action):
-        action = tf.one_hot(p_action, 16)
+            # with tf.GradientTape() as tape:
+            #     tape.watch(image)
+            #     tape.watch(action)
+            #     self.advance_recurrent_state(action)
+            #
+            #     # (
+            #     #     dynamics_loss,
+            #     #     representation_loss,
+            #     # ) = self.stochastic_timestep_to_recurrent_timestep(image)
+            #     distribution = self.dynamics_predictor.distribution(
+            #         self.recurrent_state["value"]
+            #     )
+            #
+            #     loss = self.kl_divergence(fake_dynamics, distribution)
+            #
+            #     # continue_loss = -tf.math.log(
+            #     #     self.continue_predictor.loss(self.model_state, continue_flag)
+            #     # )
+            #     # reward_loss = -tf.math.log(
+            #     #     self.reward_predictor.loss(self.model_state, reward)
+            #     # )
+            #     # decoder_loss = -tf.math.log(self.decoder.loss(self.model_state, image))
+            #
+            #     # prediction_loss = reward_loss + continue_loss + decoder_loss
+            #
+            #     # loss = (
+            #     #     DISCOUNTS["PRED"] * prediction_loss
+            #     #     + DISCOUNTS["DYN"] * dynamics_loss
+            #     #     + DISCOUNTS["REP"] * representation_loss
+            #     # )
+            #
+            #     loss = dynamics_loss
+            #
+            # variables = self.dynamics_predictor.trainable_variables
+            #
+            # tape.gradient(loss, variables)
+            # # for ellement in [
+            # #     self.dynamics_predictor,
+            # # ]:
+            # #     variables = ellement.trainable_variables
+            # #     gradient = tape.gradient(loss, variables)
+            #
+            # #     self.optimizer.apply_gradients(zip(gradient, variables))
 
-        return action
-
-    @tf.function
-    def stochastic_timestep_to_recurrent_timestep(
-        self, p_observation: Optional[Any] = None
-    ):
-        observation = tf.stack([p_observation])
-        if self.training:
-            gradient_distribution_fn = lambda model, *args: unimix_categoricals(
-                model.distribution(*args)
-            )
-
-            stop_gradient_distribution_fn = lambda model, *args: tf.stop_gradient(
-                gradient_distribution_fn(model, *args)
-            )
-
-            encoder_distribution = self.encoder.distribution(
-                self.recurrent_state["value"], observation
-            )
-
-            dynamics_distribution = gradient_distribution_fn(
-                self.dynamics_predictor, self.recurrent_state["value"]
-            )
-            encoder_distribution_stop_grad = stop_gradient_distribution_fn(
-                self.encoder, self.recurrent_state["value"], observation
-            )
-
-            dynamics_distribution_stop_grad = stop_gradient_distribution_fn(
-                self.dynamics_predictor, self.recurrent_state["value"]
-            )
-
-            self.stochastic_state["value"] = self.encoder.sample(encoder_distribution)
-            self.stochastic_state["timestep"] += 1
-
-            encoder_distribution = unimix_categoricals(encoder_distribution)
-
-            dynamics_loss = self.kl_divergence(
-                tf.ones(dynamics_distribution.shape), dynamics_distribution
-            )
-            representation_loss = self.kl_divergence(
-                encoder_distribution, dynamics_distribution_stop_grad
-            )
-
-            clipped_losses = (
-                tf.math.maximum(1.0, dynamics_loss),
-                tf.math.maximum(1.0, representation_loss),
-            )
-
-            return dynamics_loss, representation_loss
-
-        self.stochastic_state["value"] = self.dynamics_predictor(self.recurrent_state)
-        self.stochastic_state["timestep"] += 1
-
-    def advance_recurrent_state(self, action):
-        assert (
-            self.stochastic_state["timestep"] == self.recurrent_state["timestep"]
-        ), "stochastic and recurrent state are not on the same timestep"
-
-        self.recurrent_state["value"] = self.sequence_model(
-            self.recurrent_state["value"], self.stochastic_state["value"], action
-        )
-
-        self.recurrent_state["timestep"] += 1
+    # @property
+    # def model_state(self):
+    #     return tf.concat([self.recurrent_state, self.stochastic_state], axis=1)
+    #
+    # def process_action(self, p_action):
+    #     action = tf.one_hot(p_action, 16)
+    #
+    #     return action
+    #
+    # @tf.function
+    # def stochastic_timestep_to_recurrent_timestep(
+    #     self, p_observation: Optional[Any] = None
+    # ):
+    #     observation = tf.stack([p_observation])
+    #     if self.training:
+    #         gradient_distribution_fn = lambda model, *args: unimix_categoricals(
+    #             model.distribution(*args)
+    #         )
+    #
+    #         stop_gradient_distribution_fn = lambda model, *args: tf.stop_gradient(
+    #             gradient_distribution_fn(model, *args)
+    #         )
+    #
+    #         encoder_distribution = self.encoder.distribution(
+    #             self.recurrent_state["value"], observation
+    #         )
+    #
+    #         dynamics_distribution = gradient_distribution_fn(
+    #             self.dynamics_predictor, self.recurrent_state["value"]
+    #         )
+    #         encoder_distribution_stop_grad = stop_gradient_distribution_fn(
+    #             self.encoder, self.recurrent_state["value"], observation
+    #         )
+    #
+    #         dynamics_distribution_stop_grad = stop_gradient_distribution_fn(
+    #             self.dynamics_predictor, self.recurrent_state["value"]
+    #         )
+    #
+    #         self.stochastic_state["value"] = self.encoder.sample(encoder_distribution)
+    #         self.stochastic_state["timestep"] += 1
+    #
+    #         encoder_distribution = unimix_categoricals(encoder_distribution)
+    #
+    #         dynamics_loss = self.kl_divergence(
+    #             tf.ones(dynamics_distribution.shape), dynamics_distribution
+    #         )
+    #         representation_loss = self.kl_divergence(
+    #             encoder_distribution, dynamics_distribution_stop_grad
+    #         )
+    #
+    #         clipped_losses = (
+    #             tf.math.maximum(1.0, dynamics_loss),
+    #             tf.math.maximum(1.0, representation_loss),
+    #         )
+    #
+    #         return dynamics_loss, representation_loss
+    #
+    #     self.stochastic_state["value"] = self.dynamics_predictor(self.recurrent_state)
+    #     self.stochastic_state["timestep"] += 1
+    #
+    # def advance_recurrent_state(self, action):
+    #     assert (
+    #         self.stochastic_state["timestep"] == self.recurrent_state["timestep"]
+    #     ), "stochastic and recurrent state are not on the same timestep"
+    #
+    #     self.recurrent_state["value"] = self.sequence_model(
+    #         self.recurrent_state["value"], self.stochastic_state["value"], action
+    #     )
+    #
+    #     self.recurrent_state["timestep"] += 1