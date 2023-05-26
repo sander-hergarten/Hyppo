926bed263e64111803e513170d49d95d32a313e1
sander-hergarten <mail@sander-hergarten.de>
2023-05-26 21:33:56 +0200

    sdfghjk

--- src/models/recurrent_world_model/rlu_unplugged_transform.py
+++ src/models/recurrent_world_model/rlu_unplugged_transform.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 
 def batch_dataset(dataset: tf.data.Dataset):
     batch_list = [element["steps"].batch(50) for element in dataset]
-    batched_dataset = batch_list
 
-    return batched_dataset
+    for batched_dataset in batch_list:
+        print(batched_dataset.cardinality().numpy())
+        for element in batched_dataset:
+            print(element.cardinality().numpy())
+
+    return batch_list
