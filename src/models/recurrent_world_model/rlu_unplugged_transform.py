5e4e69bcf52988b378756be489f5b0b8817c5471
sander-hergarten <mail@sander-hergarten.de>
2023-05-26 21:35:33 +0200

    fsadkfashf

--- src/models/recurrent_world_model/rlu_unplugged_transform.py
+++ src/models/recurrent_world_model/rlu_unplugged_transform.py
@@ -6,7 +6,6 @@ def batch_dataset(dataset: tf.data.Dataset):
 
     for batched_dataset in batch_list:
         print(batched_dataset.cardinality().numpy())
-        for element in batched_dataset:
-            print(element.cardinality().numpy())
+        print(list(batched_dataset.take(1)))
 
     return batch_list
