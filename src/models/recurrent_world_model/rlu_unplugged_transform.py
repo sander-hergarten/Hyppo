459fc7576fb76d164c81f833ffed638f79d21d92
sander-hergarten <mail@sander-hergarten.de>
2023-05-26 21:25:11 +0200

    codfasdfa

--- src/models/recurrent_world_model/rlu_unplugged_transform.py
+++ src/models/recurrent_world_model/rlu_unplugged_transform.py
@@ -3,9 +3,6 @@ import tensorflow as tf
 
 def batch_dataset(dataset: tf.data.Dataset):
     batch_list = [element["steps"].batch(50) for element in dataset]
-    batched_dataset = batch_list.pop(0)
-
-    for batch in batch_list:
-        batched_dataset = batched_dataset.concatenate(batch)
+    batched_dataset = batch_list
 
     return batched_dataset
