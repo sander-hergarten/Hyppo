50af5379f82c9f08f5d6e36ef87235d109e23ec9
sander-hergarten <mail@sander-hergarten.de>
2023-05-26 21:45:00 +0200

    sdfghjee

--- src/models/recurrent_world_model/rlu_unplugged_transform.py
+++ src/models/recurrent_world_model/rlu_unplugged_transform.py
@@ -4,8 +4,15 @@ import tensorflow as tf
 def batch_dataset(dataset: tf.data.Dataset):
     batch_list = [element["steps"].batch(50) for element in dataset]
 
-    for batched_dataset in batch_list:
-        print(batched_dataset.cardinality().numpy())
-        print(list(batched_dataset.take(1)))
+    batched_dataset = batch_list.pop(0)
+
+    print(batched_dataset.cardinality().numpy())
+
+    for element in batch_list:
+        print("dataset size before concat", batched_dataset.cardinality().numpy())
+        print("ellements to concat", element.cardinality().numpy())
+
+        batched_dataset.concatenate(element)
+        print("dataset size after concat", batched_dataset.cardinality().numpy())
 
     return batch_list
